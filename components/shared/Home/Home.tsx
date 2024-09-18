3b9452cc1a3f896b884e980eb109a66e8c155138
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-18 21:46:08 +0300

    update

--- components/shared/Home/Home.tsx
+++ components/shared/Home/Home.tsx
@@ -16,6 +16,18 @@ const Home: FC<IHome> = ({ children }) => {
         setMounted(true);
     }, []);
 
+    useEffect(() => {
+        const setVh = () => {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        };
+
+        setVh();
+        window.addEventListener('resize', setVh);
+
+        return () => window.removeEventListener('resize', setVh);
+    }, []);
+
     const toggleSidebar = () => setSidebarIsOpen(!sidebarIsOpen);
 
     return (
