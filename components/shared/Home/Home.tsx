2d701745683efdc3525ea629fa9924b2a15bbfc9
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-18 22:05:55 +0300

    sidebar finished

--- components/shared/Home/Home.tsx
+++ components/shared/Home/Home.tsx
@@ -16,18 +16,6 @@ const Home: FC<IHome> = ({ children }) => {
         setMounted(true);
     }, []);
 
-    // useEffect(() => {
-    //     const setVh = () => {
-    //         const vh = window.innerHeight * 0.01;
-    //         document.documentElement.style.setProperty('--vh', `${vh}px`);
-    //     };
-
-    //     setVh();
-    //     window.addEventListener('resize', setVh);
-
-    //     return () => window.removeEventListener('resize', setVh);
-    // }, []);
-
     const toggleSidebar = () => setSidebarIsOpen(!sidebarIsOpen);
 
     return (
